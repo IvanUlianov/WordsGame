5286dffd35e25ad2188b9015476889c074e9345d
IvanUlianov <cokie2b@yandex.ru>
2023-06-09 14:33:46 +0300

    ?_?

--- WordsGame/Words.cs
+++ WordsGame/Words.cs
@@ -1,21 +1,22 @@
 using System.Text.RegularExpressions;
+
 namespace WordsGame
 {
-    internal class Words
-    {
-		internal static string[,] words_arr;
-        internal static char chr = ' ';
+	internal class Words
+	{
+		internal static string[,]? words_arr;
+		internal static char chr = ' ';
 		internal static string? word = "";
 
 		private static void SetWords()
-        {
+		{
 			words_arr = new string[Player.players, 1];
 		}
 		internal static void SetChar()
-            {
+		{
 			while (true)
-                {
-				Console.Write("Введите букву с которой, сегодня, играем: ");
+			{
+				Console.Write("Введите букву с которой сегодня играем: ");
 				string? InputGameChar = Console.ReadLine();
 
 				if (!string.IsNullOrEmpty(InputGameChar))
@@ -28,43 +29,43 @@ namespace WordsGame
 					{
 						chr = InputGameChar[0];
 						break;
-                }
+					}
 					else
 					{
-						Console.WriteLine($"{InputGameChar} - СЌС‚Рѕ РЅРµ Р±СѓРєРІР°");
-            }
-        }
+						Console.WriteLine($"{InputGameChar} - это не буква");
+					}
+				}
 			}
 		}
-        internal static string? CheckWords(string[,] words, string word, char gameChar)
-        {
+		internal static string? CheckWords(string[,] words, string word, char gameChar)
+		{
 			if (words == null)
 			{
 				SetWords();
 			}
 			else if (word.StartsWith(gameChar))
-            {
-                for (int i = 0; i < words.GetLength(0); i++)
-                {
-                    for (int j = 0; j < words.GetLength(1); j++)
-                    {
-                        if (words[i, j] == word)
-                        {
-                            Console.WriteLine($"РРіСЂРѕРє в„–{i + 1} СѓР¶Рµ РЅР°Р·С‹РІР°Р» СЃР»РѕРІРѕ {word}!");
+			{
+				for (int i = 0; i < words.GetLength(0); i++)
+				{
+					for (int j = 0; j < words.GetLength(1); j++)
+					{
+						if (words[i, j] == word)
+						{
+							Console.WriteLine($"Игрок №{i + 1} уже называл слово {word}!");
 							word = null;
 							return word;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine($"РЎР»РѕРІРѕ РґРѕР»Р¶РЅРѕ РЅР°С‡РёРЅР°С‚СЊСЃСЏ СЃ \'{gameChar}\' ");
+						}
+					}
+				}
+			}
+			else
+			{
+				Console.WriteLine($"Слово должно начинаться с \'{gameChar}\' ");
 				word = null;
 				return word;
-            }
-            return word;
-        }
+			}
+			return word;
+		}
 		internal static void CreateWordsArray(string[,] words, int id, string word)
 		{
 			if (words == null)
@@ -86,54 +87,54 @@ namespace WordsGame
 			}
 		}
 
-        internal static void Score(string[,] words, int players)
-        {
+		internal static void Score(string[,] words, int players)
+		{
 			if (words != null)
 			{
-            int[,] tempScore = new int[players, 1];
-            int count = 0;
+				int[,] tempScore = new int[players, 1];
+				int count = 0;
 
-            for (int i = 0; i < words.GetLength(0); i++)
-            {
-                for (int j = 0; j < words.GetLength(1); j++)
-                {
-                    if (words[i, j] != null)
-                    {
-                        count++;
-                        tempScore[i, 0] = count;
-                    }
-                }
-                count = 0;
-            }
+				for (int i = 0; i < words.GetLength(0); i++)
+				{
+					for (int j = 0; j < words.GetLength(1); j++)
+					{
+						if (words[i, j] != null)
+						{
+							count++;
+							tempScore[i, 0] = count;
+						}
+					}
+					count = 0;
+				}
 
-            for (int i = 0; i < tempScore.GetLength(0); i++)
-            {
-                for (int j = 0; j < tempScore.GetLength(1); j++)
-                {
-                    Console.Write($"РЈ РёРіСЂРѕРєР° в„–{i + 1} - {tempScore[i, j]} СЃР»РѕРІ.\n");
-                }
-            }
+				for (int i = 0; i < tempScore.GetLength(0); i++)
+				{
+					for (int j = 0; j < tempScore.GetLength(1); j++)
+					{
+						Console.Write($"У игрока №{i + 1} - {tempScore[i, j]} слов.\n");
+					}
+				}
 			}
-			else Console.WriteLine("РЅРµС‚ РёРіСЂРѕРєРѕРІ");
-        }
+			else Console.WriteLine("нет игроков");
+		}
 
-        internal static void PrintArr(string[,] words)
-        {
+		internal static void PrintArr(string[,] words)
+		{
 			if (words != null)
 			{
-            for (int i = 0; i < words.GetLength(0); i++)
-            {
+				for (int i = 0; i < words.GetLength(0); i++)
+				{
 					Console.WriteLine($"Слова игрока №{i + 1}:");
-                for (int j = 0; j < words.GetLength(1); j++)
-                {
-                    if (words[i, j] != null)
-                    {
+					for (int j = 0; j < words.GetLength(1); j++)
+					{
+						if (words[i, j] != null)
+						{
 							Console.Write($"{words[i, j]} ");
 						}
-                    }
+					}
 					Console.WriteLine();
-                }
-            }
-        }
-    }
+				}
+			}
+		}
+	}
 }
